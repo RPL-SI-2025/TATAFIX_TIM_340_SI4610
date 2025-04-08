c795284680e0a10548111acb1e2e94826d6042e8
rainasyf <rainasyifa23@gmail.com>
2025-04-08 23:25:35 +0700

    commit edit profile tukang
    Fitur Edit Profile Tukang

--- database/migrations/2025_04_08_152059_create_users_table.php
+++ database/migrations/2025_04_08_152059_create_users_table.php
@@ -12,7 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('user_id'); // Primary Key
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+
+            // Foreign Key ke tabel roles
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
