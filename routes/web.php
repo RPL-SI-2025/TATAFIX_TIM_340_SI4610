71bd139be6e5062585e27d5ec6a27aa2f5ab6d13
raihanlah <raihannoor23@gmail.com>
2025-04-19 21:59:42 +0700

    update route web
    buat bookingcontrolernya

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\BookingController;
+
 
 Route::get('/', function () {
     return view('welcome');
