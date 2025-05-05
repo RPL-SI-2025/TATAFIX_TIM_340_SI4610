be408cc642da9ae65dd97f4e59344ad0b8c35914
ignasius Jonathan <147974569+UsernamenyaApa@users.noreply.github.com>
2025-05-05 20:16:41 +0700

    membenarkan tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-      './resources/**/*.blade.php',
-      './resources/**/*.js',
-      './resources/**/*.vue',
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ['"Instrument Sans"', 'ui-sans-serif', 'system-ui'],
-        },
-      },
-    },
-    plugins: [],
-  }
-  
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
