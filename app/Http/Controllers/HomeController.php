09a0260e564e374cb929f0d5417d3167da2e348a
ignasius Jonathan <147974569+UsernamenyaApa@users.noreply.github.com>
2025-05-07 02:09:54 +0700

    mengupdate tampilan dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        // Ambil 3 layanan terbaru untuk ditampilkan di homepage
+        $services = Service::with('category')
+                    ->where('availbility', true)
+                    ->latest()
+                    ->take(3)
+                    ->get();
+                    
+        return view('pages.home', compact('services'));
     }
 }
